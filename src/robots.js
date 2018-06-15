b3eefcb18cf1145f958a34d2bfa8b423fb3ee0cf
A-Sm1th <ronald.fortmann@gmail.com>
2018-06-15 09:04:44 +0200

    Debug files

---
+++ src/robots.js
@@ -0,0 +1,20 @@
+export const robots = [
+    {
+        id: 1,
+        name: "Bernard de la Villardière",
+        username: "Bernie",
+        email: "bernielefou@m6.fr"
+    },
+    {
+        id: 2,
+        name: "François-Henri des Maréchaussés",
+        username: "s3xy_francois",
+        email: "s3xy_francois@francetv.fr"
+    },
+    {
+        id: 3,
+        name: "Celine Dion",
+        username: "tanerbak_bitch",
+        email: "jaimeletabernak@cbc.ca"
+    }
+]
